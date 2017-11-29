7ae37700c523aa09eb8d4a62d4b9cdb98a96ecba
REMIR Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-11-29 15:09:40 +0100

    open_json function

--- main.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-data = np.loadtxt('./test_data_Training_Data.csv', delimiter=",")
-print(data.shape)
---
+++ open_json.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from pprint import pprint
+import os
+import json
+
+root = './data/'
+
+def json_path(root):
+    ext = '.json'
+    list_json_data = []
+    for file in os.listdir(root):
+        if file.endswith(ext):
+            list_json_data.append(json.load(open(os.path.join(root, file))))
+        else:
+            print('file not json')
+    return list_json_data
+pass
+
+
+def open_json_array(list_data):
+    for json_path in list_data:
+        pprint(json.load(open(json_path)))
+pass
+
+
+list_data = json_path(root)
+
+open_json_array(list_data)

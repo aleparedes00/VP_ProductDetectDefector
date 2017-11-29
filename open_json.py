0776f9a7eea7e59faeeac7fa3df828e4c068880b
REMIR Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-11-29 18:00:23 +0100

    open_json: remove test fonc & var

--- open_json.py
+++ open_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
+
 from pprint import pprint
 import os
 import json
-
-root = './data/'
 
 def json_path(root):
     ext = '.json'
@@ -21,8 +20,3 @@ def open_json_array(list_data):
     for json_path in list_data:
         print(json.load(open(json_path)))
 pass
-
-
-list_data = json_path(root)
-
-open_json_array(list_data)

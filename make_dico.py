116e843ab3a0283af821a768e77731c28ff751e0
REMIR Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-11-30 15:08:26 +0100

    make dico feature targets target_last

--- make_dico.py
+++ make_dico.py
@@ -7,7 +7,7 @@ import json
 faire un dictionner qui lit chaque ID a un TEXT a partir de fichier json
 
 """
-def make_dico(path):
+def make_dico_target(path):
     dico = dict()
     file = open(path, encoding='utf-8').read()
     json_file = json.loads(file)['fts']
@@ -15,6 +15,29 @@ def make_dico(path):
         for j in range(0, len(json_file[i]['classification'])):
             dico[json_file[i]['classification'][j]['id']] =  json_file[i]['classification'][j]['text']
         pass
+    pass
+    return dico
+pass
+
+def make_dico_feature(path):
+    dico = dict()
+    file = open(path, encoding='utf-8').read()
+    json_file = json.loads(file)['fts']
+    for i in range(0, len(json_file)):
+        for j in range(0, len(json_file[i]['attributes'])):
+            dico[json_file[i]['attributes'][j]['id']] =  json_file[i]['attributes'][j]['text']
+        pass
+    pass
+    return dico
+pass
+
+def make_dico_target_last(path):
+    dico = dict()
+    file = open(path, encoding='utf-8').read()
+    json_file = json.loads(file)['fts']
+    for i in range(0, len(json_file)):
+        j = len(json_file[i]['classification'])
+        dico[json_file[i]['classification'][j]['id']] =  json_file[i]['classification'][j]['text']
     pass
     return dico
 pass

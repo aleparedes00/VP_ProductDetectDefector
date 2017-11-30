5317b0bd34a395d431ebc66ec250175afcd22828
Arastal <berger_a@etna-alternance.net>
2017-11-30 18:18:03 +0100

    Add : make_training_data(file), Add : Pourcentage de précision de l'IA.

--- learning.py
+++ learning.py
@@ -10,6 +10,7 @@ targets = []
 test_data = []
 check_data = []
 class_dict = dict()
+
 # Chargement de la liste des fichiers
 json_files = json_path('data')
 
@@ -17,8 +18,8 @@ json_files = json_path('data')
 for file in json_files:
 
     # Construction des listes de données
-    tmp_features, tmp_targets, tmp_test_data, tmp_check_data = make_test_data(file)
-
+    tmp_features, tmp_targets = make_training_data(file)
+    tmp_test_data, tmp_check_data = make_test_data(file)
 
     # Construction du dictionnaire des classifications
     tmp_class_dict = make_dico_target(file)
@@ -37,6 +38,8 @@ features, test_data = uniformise_features(features, test_data)
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, targets)
 
+nb_error = 0
+
 # Évaluation des données de test
 for i in range(len(test_data)):
     answer = clf.predict([test_data[i]])[0]
@@ -44,6 +47,11 @@ for i in range(len(test_data)):
     if answer != check:
         print("Je pense que ce produit a pour catégorie... " + class_dict[answer])
         print("Le produit est en réalité dans la catégorie... " + class_dict[check])
-        # TODO: intercepter les erreurs et calculer le pourcentage de précision de la machine.
         print("\033[91mERROR\033[0m")
         print("")
+        nb_error = nb_error + 1
+
+# Pourcentage de précision de la machine
+raw_precision = ((len(test_data) - nb_error) / len(test_data))*100
+precision = format(raw_precision, '.2f')
+print("Le pourcentage de précision est de \033[92m" + str(precision) + "%.\033[0m")

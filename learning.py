9fed6c471ddf50b5a18430e7128a685a6e283c6e
Alexandre H <hyvern_a@etna-alternance.net>
2017-12-01 09:30:04 +0100

    updated error handling

--- learning.py
+++ learning.py
@@ -10,64 +10,67 @@ from make_dico import *
 from sklearn import tree
 import pickle
 
-# initialistion liste de données global
-features = []
-targets = []
-eval_features = []
-eval_targets = []
-eval_info = []
-class_dict = dict()
+if os.path.isdir('data'):
+    # initialistion liste de données global
+    features = []
+    targets = []
+    eval_features = []
+    eval_targets = []
+    eval_info = []
+    class_dict = dict()
 
-# Chargement de la liste des nouveaux fichiers référence (dossier data)
-json_files = json_path('data')
+    # Chargement de la liste des nouveaux fichiers référence (dossier data)
+    json_files = json_path('data')
 
-# Parcours des fichiers
-for file in json_files:
+    # Parcours des fichiers
+    for file in json_files:
 
-    # Construction des listes de données
-    tmp_features, tmp_targets, tmp_info = make_data(file)
-    tmp_eval_features, tmp_eval_targets, tmp_eval_info = make_test_data(file)
+        # Construction des listes de données
+        tmp_features, tmp_targets, tmp_info = make_data(file)
+        tmp_eval_features, tmp_eval_targets, tmp_eval_info = make_test_data(file)
 
-    # Construction du dictionnaire des classifications
-    tmp_class_dict = make_dico_target_last(file)
+        # Construction du dictionnaire des classifications
+        tmp_class_dict = make_dico_target_last(file)
 
-    # update des liste de donnée global
-    features.extend(tmp_features)
-    targets.extend(tmp_targets)
-    eval_features.extend(tmp_eval_features)
-    eval_targets.extend(tmp_eval_targets)
-    class_dict.update(tmp_class_dict)
+        # update des liste de donnée global
+        features.extend(tmp_features)
+        targets.extend(tmp_targets)
+        eval_features.extend(tmp_eval_features)
+        eval_targets.extend(tmp_eval_targets)
+        class_dict.update(tmp_class_dict)
 
-# Uniformisation des listes (pour que les lignes aient toutes la même longueur)
-features, eval_features = uniformise_features(features, eval_features)
+    # Uniformisation des listes (pour que les lignes aient toutes la même longueur)
+    features, eval_features = uniformise_features(features, eval_features)
 
-# Construction de l'arbre de décision
-model = tree.DecisionTreeClassifier()
-model = model.fit(features, targets)
+    # Construction de l'arbre de décision
+    model = tree.DecisionTreeClassifier()
+    model = model.fit(features, targets)
 
-nb_error = 0
+    nb_error = 0
 
-# Évaluation des données de test
-for i in range(len(eval_features)):
-    answer = model.predict([eval_features[i]])[0]
-    check = eval_targets[i]
-    if answer != check:
-        print("Je pense que ce produit a pour catégorie... " + class_dict[answer])
-        print("Le produit est en réalité dans la catégorie... " + class_dict[check])
-        print("Le objet est l'id =" + eval_info[i]['id'] + "dans le fichier " + eval_info[i]['file'] + "\n")
-        print("\033[91mERROR\033[0m\n")
-        nb_error = nb_error + 1
+    # Évaluation des données de test
+    for i in range(len(eval_features)):
+        answer = model.predict([eval_features[i]])[0]
+        check = eval_targets[i]
+        if answer != check:
+            print("Je pense que ce produit a pour catégorie... " + class_dict[answer])
+            print("Le produit est en réalité dans la catégorie... " + class_dict[check])
+            print("Le objet est l'id =" + eval_info[i]['id'] + "dans le fichier " + eval_info[i]['file'] + "\n")
+            print("\033[91mERROR\033[0m\n")
+            nb_error = nb_error + 1
 
-# Pourcentage de précision de la machine
-if len(eval_features) > 0:
-    raw_precision = ((len(eval_features) - nb_error) / len(eval_features))*100
-    precision = format(raw_precision, '.2f')
-    print("Le pourcentage de précision est de \033[92m" + str(precision) + "%.\033[0m")
-    print("Nombre d'erreurs : " + str(nb_error) + ".\n")
+    # Pourcentage de précision de la machine
+    if len(eval_features) > 0:
+        raw_precision = ((len(eval_features) - nb_error) / len(eval_features))*100
+        precision = format(raw_precision, '.2f')
+        print("Le pourcentage de précision est de \033[92m" + str(precision) + "%.\033[0m")
+        print("Nombre d'erreurs : " + str(nb_error) + ".\n")
 
-# Sauvegarde du modèle
+    # Sauvegarde du modèle
+    else:
+        pickle.dump(features, open('model/features.sav', 'wb'))
+        pickle.dump(targets, open('model/targets.sav', 'wb'))
+        pickle.dump(class_dict, open('model/class_dict.sav', 'wb'))
+        print("Modèle sauvegardé.")
 else:
-    pickle.dump(features, open('model/features.sav', 'wb'))
-    pickle.dump(targets, open('model/targets.sav', 'wb'))
-    pickle.dump(class_dict, open('model/class_dict.sav', 'wb'))
-    print("Modèle sauvegardé.")
+    print("No data directory found.")
--- main.py
+++ main.py
@@ -6,54 +6,60 @@ import pickle
 import os
 from sklearn import tree
 
-try:
-    # AP creating all list for features, targets and info that will be submit at test
-    features = pickle.load(open('model/features.sav', 'rb'))
-    targets = pickle.load(open('model/targets.sav', 'rb'))
-    class_dict = pickle.load(open('model/class_dict.sav', 'rb'))
-    eval_features = []
-    eval_targets = []
-    eval_info = []
+if os.path.isdir('eval'):
+    try:
+        # AP creating all list for features, targets and info that will be submit at test
+        features = pickle.load(open('model/features.sav', 'rb'))
+        targets = pickle.load(open('model/targets.sav', 'rb'))
+        class_dict = pickle.load(open('model/class_dict.sav', 'rb'))
+        eval_features = []
+        eval_targets = []
+        eval_info = []
 
-    json_files = json_path('eval')
+        json_files = json_path('eval')
 
-    for file in json_files:
+        if len(json_files) == 0:
+            print("No files to evaluate.")
+        else:
+            for file in json_files:
 
-        # creating the data lists
-        tmp_eval_features, tmp_eval_targets, tmp_eval_info = make_data(file)
+                # creating the data lists
+                tmp_eval_features, tmp_eval_targets, tmp_eval_info = make_data(file)
 
-        # Construction du dictionnaire des classifications
-        tmp_class_dict = make_dico_target_last(file)
+                # Construction du dictionnaire des classifications
+                tmp_class_dict = make_dico_target_last(file)
 
-        eval_features.extend(tmp_eval_features)
-        eval_targets.extend(tmp_eval_targets)
-        eval_info.extend(tmp_eval_info)
-        class_dict.update(tmp_class_dict)
+                eval_features.extend(tmp_eval_features)
+                eval_targets.extend(tmp_eval_targets)
+                eval_info.extend(tmp_eval_info)
+                class_dict.update(tmp_class_dict)
 
-    # Uniformisation des listes (pour que les lignes aient toutes la même longueur)
-    features, eval_features = uniformise_features(features, eval_features)
+            # Uniformisation des listes (pour que les lignes aient toutes la même longueur)
+            features, eval_features = uniformise_features(features, eval_features)
 
-    # Construction de l'arbre de décision
-    model = tree.DecisionTreeClassifier()
-    model = model.fit(features, targets)
+            # Construction de l'arbre de décision
+            model = tree.DecisionTreeClassifier()
+            model = model.fit(features, targets)
 
-    # Évaluation des données de test
-    nb_error = 0
-    log = open("error_log.txt", "w+", encoding="UTF-8")
-    for i in range(len(eval_features)):
-        answer = model.predict([eval_features[i]])[0]
-        check = eval_targets[i]
-        if answer != check:
-            log.write("Expected [" + class_dict[answer] + "], found [" + class_dict[check] + "] for product [" +
-                      str(eval_info[i]['id']) + " - " + str(eval_info[i]['file']) + "]\n")
-            nb_error = nb_error + 1
-    log.close()
+            # Évaluation des données de test
+            nb_error = 0
+            log = open("error_log.txt", "w+", encoding="UTF-8")
+            for i in range(len(eval_features)):
+                answer = model.predict([eval_features[i]])[0]
+                check = eval_targets[i]
+                if answer != check:
+                    log.write("Expected [" + class_dict[answer] + "], found [" + class_dict[check] + "] for product [" +
+                              str(eval_info[i]['id']) + " - " + str(eval_info[i]['file']) + "]\n")
+                    nb_error = nb_error + 1
+            log.close()
 
-    if nb_error > 0:
-        print("Found " + str(nb_error) + " errors. Check error_log.txt for more info.")
-    else:
-        os.remove("error_log.txt")
-        print("No error found.")
+            if nb_error > 0:
+                print("Found " + str(nb_error) + " errors. Check error_log.txt for more info.")
+            else:
+                os.remove("error_log.txt")
+                print("No error found.")
 
-except FileNotFoundError:
-    print("Classification model undefined. Please run learning.py script first.")
+    except FileNotFoundError:
+        print("Classification model undefined. Please run learning.py script first.")
+else:
+    print("No eval directory found.")
